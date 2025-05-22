f94cf19799136203b9e306a2712b4cee344dc723
Orca <tundraorca.tech@gmail.com>
2025-05-22 15:50:50 +0100

    spark integration

--- postgres/init.sql
+++ postgres/init.sql
@@ -1,16 +1,2 @@
-CREATE TABLE IF NOT EXISTS public.postg_ipca (
-  id SERIAL PRIMARY KEY,
-  nome TEXT,
-  valor NUMERIC,
-  dt_update TIMESTAMP
-);
-
-CREATE TABLE IF NOT EXISTS public.postg_pre (
-  id SERIAL PRIMARY KEY,
-  nome TEXT,
-  valor NUMERIC,
-  dt_update TIMESTAMP
-);
-
 TRUNCATE TABLE public.postg_ipca;
 TRUNCATE TABLE public.postg_pre;
